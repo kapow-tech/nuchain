0935d69d371ab0a4f11294278a1d565de9c97b14
jinuthankachan <jinu.t.92@gmail.com>
2024-11-04 23:07:30 +0530

    push data

--- x/audit/keeper/msg_server_push_data.go
+++ x/audit/keeper/msg_server_push_data.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 
+	"nuchain/api/nuchain/audit"
 	"nuchain/x/audit/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -13,6 +14,17 @@ func (k msgServer) PushData(goCtx context.Context, msg *types.MsgPushData) (*typ
 
 	// TODO: Handling the message
 	_ = ctx
+	data := audit.Entry{
+		Actor:     msg.Actor,
+		Datatype:  msg.DataType,
+		Id:        msg.Id,
+		Data:      msg.Data,
+		Timestamp: msg.Timestamp,
+	}
+	err := k.Db.EntryTable().Insert(ctx, &data)
+	if err != nil {
+		return nil, err
+	}
 
 	return &types.MsgPushDataResponse{}, nil
 }
